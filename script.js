fad3c00bc3b3baed7d2d40b9bb19ba806fb4dc65
Tatjana Oljaca <tatjanaoljaca@gmail.com>
2022-11-21 18:48:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+/*const body = document.body
+
+const getRandomNumber = (num) => {
+    return Math.floor(Math.random()*num)
+}
+
+const getRandomColor = () => {
+    const h = getRandomNumber(360)
+    const s = getRandomNumber(100)
+    const l = getRandomNumber(100)
+
+    return `hsl(${h}deg, ${s}%, ${l}%)`
+}
+
+const setBackgroundColor = () => {
+    const randomColor = getRandomColor();
+    body.style.backgroundColor = randomColor;
+    body.style.color = randomColor;
+  };
+   
+  setBackgroundColor();
+
+window.onload = () => {
+    setBackgroundColor()
+}*/
+
+/*function random_bg_color() {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")"
+ console.log(bgColor)
+  
+    document.body.style.background = bgColor
+    }
+
+random_bg_color()*/
+
+/*const colors = ['#eca1a6', '#b5e7a0', '#eea29a'] 
+const body = document.body
+
+function changeColor () {
+    let backgroundColor = Math.floor(Math.random() * (colors.length))
+    body.style.background = backgroundColor
+
+}
+
+changeColor()
+
+document.addEventListener('load', changeColor())*/
+
+
